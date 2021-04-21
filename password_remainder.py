261245cea2de4046b36974f9303ca163386ea200
cewik <cewik0770@gmail.com>
2021-04-21 17:31:52 +0300

    008

---
+++ password_remainder.py
@@ -0,0 +1,7 @@
+stored_name = "Joseph"
+stored_password = "W@12"
+name = input("Please enter your first name: ")
+if name == stored_name:
+  print("Hello, Joseph! The password is:", stored_password)
+else:
+   print("Hello,", name, "See you later.")
